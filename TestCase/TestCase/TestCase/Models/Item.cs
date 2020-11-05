c2d77874cb0c7a59a62903dbedbd5494e88ae103
saadlamkadam <saad.lamkadam@gmail.com>
2020-11-05 21:40:47 +0100

    Added the Item Class

---
+++ TestCase/TestCase/TestCase/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestCase.Models
+{
+    public class Item
+    {
+        public int ItemCode { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public string CustomerDescription { get; set; }
+        public bool SalesItem { get; set; }
+        public bool StockItem { get; set; }
+        public bool PurchasedItem { get; set; }
+        public string Barcode { get; set; }
+        public enum ManagedBy { None = 1, Serial = 2, Batch = 3 }
+        public double MinimumInventory { get; set; }
+        public double MaximumInventory { get; set; }
+        public string Remarks { get; set; }
+        public string ImagePath { get; set; }
+
+
+    }
+}
---
+++ TestCase/TestCase/TestCase/Models/TestCaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TestCase.Models
+{
+    public class TestCaseContext : DbContext
+    {
+
+    }
+}
